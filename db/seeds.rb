acd64a5293cf8aba7ba55ffab8c023a087fdeec5
dustin <dandersonsoftware@outlook.com>
2018-01-22 21:27:16 -0500

    index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    content: "My fake content"
+  )
+end
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skills}",
+    percent_utilized: 15
+  )
+end
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "My fake portfolio body",
+    main_image: "",
+    thumb_image: "https://placehold.it/350x150"
+  )
