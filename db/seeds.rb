f33ae1e14550137da820b0ea543850c384253c70
dustin <dandersonsoftware@outlook.com>
2018-01-22 21:54:55 -0500

    new and create action for portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    content: "My fake content"
+    body: "My fake content"
   )
 end
 
 5.times do |skill|
   Skill.create!(
-    title: "Rails #{skills}",
+    title: "Rails #{skill}",
     percent_utilized: 15
   )
 end
@@ -17,6 +17,7 @@ end
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "My great service",
     body: "My fake portfolio body",
-    main_image: "",
+    main_image: "https://placehold.it/600x400",
     thumb_image: "https://placehold.it/350x150"
   )
+end
