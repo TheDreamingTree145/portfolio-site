eac0427f6ed1c6fc8cac348fb973d3fac26cf231
dustin <dandersonsoftware@outlook.com>
2018-03-22 02:13:54 -0400

    fixed issue with action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  root to: 'pages#home'
   resources :portfolios do
     put :sort, on: :collection
   end
-  root to: 'pages#home'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   end
 
   mount ActionCable.server => '/cable'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
