ab26c309f17e087b7809f2a974b3f676fc1c0283
dustin <dandersonsoftware@outlook.com>
2018-05-08 04:53:11 -0400

    configured action cable for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://da-portfolio-site.herokuapp.com', 'http://da-portfolio-site.herokuapp.com']
+  config.action_cable.url = 'wss://da-portfolio-site.herokuapp.com/cable'
 end
